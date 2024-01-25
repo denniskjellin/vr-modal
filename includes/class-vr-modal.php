818b98b667986fd2b152ef6c9cc3aa9978cb8443
Dennis Kjellin <dennis.kjellin@knowit.se>
2024-01-25 09:55:25 +0100

    comments

--- includes/class-vr-modal.php
+++ includes/class-vr-modal.php
@@ -8,13 +8,23 @@
 
  use knowit\helper\Util;
 class Vr_Modal {
-	// Define plugin constants
+	/**
+	 * Plugin ID.
+	 *
+	 * @var string
+	 */
 	const ID = 'vr-modal';
 
-	// Define custom post types
+	/**
+	 * Text domain.
+	 *
+	 * @var string
+	 */
 	const TEXT_DOMAIN = 'vr-modal';
 
-	// Define plugin variables
+	/**
+	 * Constructor.
+	 */
 	public function __construct() {
 		// register custom post type
 		add_action('init', array($this, 'register_custom_post_type'));
@@ -33,14 +43,15 @@ class Vr_Modal {
 
 		//change title hook
 		add_filter( 'enter_title_here', array( $this, 'change_post_title_placeholder' ), 20, 2 );
+
+		// Add custom column to the admin table
+		add_action('admin_init', array($this, 'manage_columns'));
 		
 		Util::logger('Vr_Modal class loaded');
 	}
 	
 
-
-
-	// validation script for custom post type (jquery)
+	// Enqueue validation script for custom post type (jquery)
 	public function validate_script(){    
   	wp_enqueue_script('vr-validate', plugin_dir_url(__FILE__) . 'jquery-validation-1.19.5/dist/jquery.validate.min.js', array('jquery'));
   	wp_enqueue_script('vr-validate-admin-script', plugin_dir_url(__FILE__) . 'vr-modal-validation.js', array('jquery','vr-validate'));
@@ -76,7 +87,13 @@ class Vr_Modal {
 		<?php
 	}
 
-	// Custom title placeholder for infobox. */
+/**
+ * Change title placeholder text.
+ *
+ * @param string $title The original title placeholder text.
+ * @param WP_Post $post The post object.
+ * @return string
+ */
 public function change_post_title_placeholder( $title, $post ) {
     $post_type = 'vr_modal_post_type';
 
@@ -88,7 +105,12 @@ public function change_post_title_placeholder( $title, $post ) {
     return $title;
 }
 
-// save function for custom post type	
+/**
+ * Save post meta.
+ *
+ * @param int $post_id The post ID.
+ * @param WP_Post $post The post object.
+ */	
 public function save_post($post_id, $post)
 {
 	// Add nonce for security and authentication.
@@ -99,7 +121,7 @@ public function save_post($post_id, $post)
         return false;
     }
 
-	  // check if form is set or not.
+	// check if form is set or not.
 	if (!isset($_POST['vrm_title']) || !isset($_POST['vrm_content']) || !isset($_POST['vrm_button_title']) || !isset($_POST['vrm_button_url'])) {
 		return false;
 	}
@@ -111,7 +133,6 @@ public function save_post($post_id, $post)
     update_post_meta($post_id, 'vrm_button_url', sanitize_text_field($_POST['vrm_button_url']));
 }
 
-	// Add meta box to custom post type
 	public function add_meta_box( ){
 		add_meta_box(
 			'vr-modal-meta-box',
@@ -123,35 +144,6 @@ public function save_post($post_id, $post)
 		);
 	}
 
-public function render_meta_box( $post ){
-    // Add nonce for security and authentication.
-    wp_nonce_field('vrm_meta_box_nonce', 'vrm_meta_box_nonce');
-    ?>
-    <!-- Add fields for data entry. -->
-    <div class="vr-modal-box">
-        <label for="vrm-title">Ange rubrik:</label><br>
-        <input name="vrm_title" type="text" placeholder="Rubriken som syns i modalen för besökaren" id="vrm-title" value="<?php echo get_post_meta( $post->ID, 'vrm_title', true );?>" style="width: 100%;">
-    </div> 
-    <div class="vr-modal-box">
-        <label for="vrm-content">Ange innehållet:</label><br>
-        <textarea name="vrm_content" rows="10" cols="" placeholder="Textuellt innehåll som syns för besökaren" id="vrm_content" class="large-text" style="width: 100%;"><?php echo esc_textarea(get_post_meta($post->ID, 'vrm_content', true)); ?></textarea>
-    </div>
-    <div class="vr-modal-box">
-        <label for="vrm_button_title">Ange rubrik för länken:</label><br>
-        <input name="vrm_button_title" type="text" placeholder="Vidare till nyhetsbrevet" id="vrm_button_title" value="<?php echo get_post_meta( $post->ID, 'vrm_button_title', true );?>" style="width: 100%;">
-    </div>
-    <div class="vr-modal-box">
-        <label for="vrm_button_url">Ange URL för länk:</label><br>
-        <input name="vrm_button_url" type="text" placeholder="https://www.genteknik.se/nyhetsbrev" id="vrm_button_url" value="<?php echo get_post_meta( $post->ID, 'vrm_button_url', true );?>" style="width: 100%;">
-    </div>
-    <div class="vr-modal-box">
-        <p class="description">* samtliga fält är obligatoriska.</p>
-    </div>
-    <?php
-}
-
-
-	// Define custom post types
 	public function register_custom_post_type() {
 		$menu_slug = $this->get_id();
 
@@ -191,65 +183,119 @@ public function render_meta_box( $post ){
 	}
 
 
-	// Define plugin variables
-	public function init()
-	{
-		// Add menu page
-		add_action('admin_menu', array($this, 'add_menu_page'), 1);
-		add_action('admin_init', array($this, 'register_settings'));
 
-		// Register REST API endpoint
-		add_action('rest_api_init', array($this, 'register_rest_endpoint'));
+/**
+ * Display custom column content
+ *
+ * @param string $column
+ * @param int $post_id
+ */
+public function display_custom_column_content($column, $post_id) {
+	if ($column === 'vrm_title_column' && get_post_type($post_id) === 'vr_modal_post_type') {
+		// Get the value of the custom field 'vrm_title'
+		$vrm_title = get_post_meta($post_id, 'vrm_title', true);
+
+		// Output the custom field value
+		echo esc_html($vrm_title);
 	}
+}
 
-	// Define plugin functions
-	public function get_id()
-	{
-		return self::ID;
-	}
+/**
+ * Add custom column to the admin table
+ *
+ * @param array $columns
+ * @return array
+ */
 
+public function add_custom_column($columns) {
+	$columns['vrm_title_column'] = 'Modal rubrik';
+	return $columns;
+}
+
+
+public function manage_columns() {
+	add_filter('manage_vr_modal_post_type_posts_columns', array($this, 'add_custom_column'));
+	add_action('manage_vr_modal_post_type_posts_custom_column', array($this, 'display_custom_column_content'), 10, 2);
+}
+
+
+/**
+ * Render meta box content.
+ *
+ * @param WP_Post $post The post object.
+ */
+public function init()
+{
 	// Add menu page
-	public function add_menu_page()
-	{
-		// Check if the menu page already exists
-		$menu_page_exists = false;
-		global $menu;
-		foreach ($menu as $item) {
-			if ($item[2] === $this->get_id()) {
-				$menu_page_exists = true;
-				break;
-			}
-		}
-
-		// If the menu page doesn't exist, add it
-		if (!$menu_page_exists) {
-			add_menu_page(
-				esc_html__('VR Modal', 'vr-modal-admin'),
-				esc_html__('VR Modal', 'vr-modal-admin'),
-				'manage_options',
-				$this->get_id(),
-				array($this, 'load_view'),
-				'dashicons-admin-page'
-			);
-		}
-	}
-
-	// Register settings
-	public function register_settings()
-	{
-		register_setting($this->get_id() . '_settings_group', $this->get_id() . '_settings_data');
-	}
+	add_action('admin_menu', array($this, 'add_menu_page'), 1);
+	add_action('admin_init', array($this, 'register_settings'));
 
 	// Register REST API endpoint
-	public function register_rest_endpoint() {
-		register_rest_route('vr-modal/v1', '/modal-data', array(
-			'methods' => 'GET',
-			'callback' => array($this, 'get_modal_data'),
-			'permission_callback' => '__return_true',
-		));
+	add_action('rest_api_init', array($this, 'register_rest_endpoint'));
+}
+
+/**
+ * Get the plugin ID.
+ *
+ * @return string
+ */
+public function get_id()
+{
+	return self::ID;
+}
+
+
+/**
+ * Render meta box content.
+ *
+ * @param WP_Post $post The post object.
+ */
+public function add_menu_page()
+{
+	// Check if the menu page already exists
+	$menu_page_exists = false;
+	global $menu;
+	foreach ($menu as $item) {
+		if ($item[2] === $this->get_id()) {
+			$menu_page_exists = true;
+			break;
+		}
 	}
 
-// Get modal data
+	// If the menu page doesn't exist, add it
+	if (!$menu_page_exists) {
+		add_menu_page(
+			esc_html__('VR Modal', 'vr-modal-admin'),
+			esc_html__('VR Modal', 'vr-modal-admin'),
+			'manage_options',
+			$this->get_id(),
+			array($this, 'load_view'),
+			'dashicons-admin-page'
+		);
+	}
+}
+
+// Register settings
+public function register_settings()
+{
+	register_setting($this->get_id() . '_settings_group', $this->get_id() . '_settings_data');
+}
+
+// Register REST API endpoint
+public function register_rest_endpoint() 
+{
+	register_rest_route('vr-modal/v1', '/modal-data', array(
+		'methods' => 'GET',
+		'callback' => array($this, 'get_modal_data'),
+		'permission_callback' => '__return_true',
+	));
+}
+
+/**
+ * Get modal data.
+ *
+ * @return WP_REST_Response
+ */
 public function get_modal_data() {
     $enable_feature = get_option($this->get_id() . '_settings_data')['enable_feature'] ?? 0;
 
@@ -276,8 +322,41 @@ public function get_modal_data() {
     }
 }
 
+/**
+ * Render meta box content.
+ *
+ * @param WP_Post $post The post object.
+ */
 
-	// Load view - wp admin
+
+public function render_meta_box( $post ){
+    // Add nonce for security and authentication.
+    wp_nonce_field('vrm_meta_box_nonce', 'vrm_meta_box_nonce');
+    ?>
+    <!-- Add fields for data entry. -->
+    <div class="vr-modal-box">
+        <label for="vrm-title">Ange rubrik:</label><br>
+        <input name="vrm_title" type="text" placeholder="Rubriken som syns i modalen för besökaren" id="vrm-title" value="<?php echo get_post_meta( $post->ID, 'vrm_title', true );?>" style="width: 100%;">
+    </div> 
+    <div class="vr-modal-box">
+        <label for="vrm-content">Ange innehållet:</label><br>
+        <textarea name="vrm_content" rows="10" cols="" placeholder="Textuellt innehåll som syns för besökaren" id="vrm_content" class="large-text" style="width: 100%;"><?php echo esc_textarea(get_post_meta($post->ID, 'vrm_content', true)); ?></textarea>
+    </div>
+    <div class="vr-modal-box">
+        <label for="vrm_button_title">Ange rubrik för länken:</label><br>
+        <input name="vrm_button_title" type="text" placeholder="Vidare till nyhetsbrevet" id="vrm_button_title" value="<?php echo get_post_meta( $post->ID, 'vrm_button_title', true );?>" style="width: 100%;">
+    </div>
+    <div class="vr-modal-box">
+        <label for="vrm_button_url">Ange URL för länk:</label><br>
+        <input name="vrm_button_url" type="text" placeholder="https://www.genteknik.se/nyhetsbrev" id="vrm_button_url" value="<?php echo get_post_meta( $post->ID, 'vrm_button_url', true );?>" style="width: 100%;">
+    </div>
+    <div class="vr-modal-box">
+        <p class="description">* samtliga fält är obligatoriska.</p>
+    </div>
+    <?php
+}
+
+// Render the menu page view.
 public function load_view()
 {
     ?>
@@ -311,6 +390,7 @@ public function load_view()
             <?php do_settings_sections($this->get_id() . '_settings_group'); ?>
 			<br><h2><?php esc_html_e('Inställningar', 'vr-modal-admin'); ?></h2>
 				<p>* Modalen visas endast när funktionen är aktiverad.</p>
+				<p>* Slå av funktionen om ingen modal ska användas.</p>
 				            <table class="form-table">
                 <!-- Toggle switch for enabling/disabling feature -->
                 <tr>
@@ -326,4 +406,3 @@ public function load_view()
     <?php
  }
 }
-
